8b6d899ecf43cfc3c6e644199cd05f6f01e7d1d7
eskaya <esra.kaya@bil.omu.edu.tr>
2020-09-13 17:23:28 +0300

    Lesson-3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoapp';
+
+  user = 'Esra';
+
+  items = [
+    {description: "Kahvaltı", action: "No"},
+    {description: "Spor", action: "No"},
+    {description: "Sinema", action: "No"},
+    {description: "Fatura", action: "No"},
+  ];
 }
